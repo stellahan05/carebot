3f45832fceef644e17118f288f3daca8c5edfe88
Stella Han <stellahan0819@gmail.com>
2025-02-28 22:14:30 -0800

    deploy backend to render

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,16 @@ import chatbotRoutes from "./routes/chatbotRoutes.js";
 dotenv.config();
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// routes
 app.use("/api", chatbotRoutes);
 
-app.listen(5001, () => console.log("Server running on port 5001"));
+// dynamic port for Render or fallback to 5001 for local dev
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log('Server running on port ${port}');
+});
